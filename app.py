bd6112f950d3737fb3181b73a9f62fe7a3c501b9
CAAAB <23194027+CAAAB@users.noreply.github.com>
2023-12-27 07:23:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, jsonify
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -7,80 +7,6 @@ import requests
 
 smtp_username = os.environ.get('SMTP_USERNAME')
 smtp_password = os.environ.get('SMTP_PASSWORD')
-
-simple_form = """<!DOCTYPE html>
-<html>
-<head>
-    <title>Email Form</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f4f4f4;
-        }
-        form {
-            background: white;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        input[type=text], input[type=email], textarea {
-            width: 100%;
-            padding: 8px;
-            margin: 8px 0;
-            display: inline-block;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            box-sizing: border-box;
-        }
-        input[type=submit] {
-            width: 100%;
-            background-color: #4CAF50;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-        input[type=submit]:hover {
-            background-color: #45a049;
-        }
-        label {
-            margin-top: 10px;
-            display: block;
-        }
-    </style>
-</head>
-<body>
-
-    <form method="POST" action="/send-email">
-        <label for="from_name">From Name:</label>
-        <input type="text" id="from_name" name="from_name">
-        
-        <label for="from_email">From Email:</label>
-        <input type="email" id="from_email" name="from_email">
-        
-        <label for="subject">Subject:</label>
-        <input type="text" id="subject" name="subject">
-        
-        <label for="body">Body:</label>
-        <textarea id="body" name="body"></textarea>
-        
-        <label for="recipient">Recipient:</label>
-        <input type="email" id="recipient" name="recipient">
-        
-        <input type="submit" value="Send Email">
-    </form>
-
-</body>
-</html>
-"""
 
 better_form = """
 <!DOCTYPE html>
@@ -140,6 +66,7 @@ better_form = """
         
         <label for="from_email">From Email:</label>
         <input type="email" id="from_email" name="from_email">
+        <span id="domain_indicator"></span>
         
         <label for="subject">Subject:</label>
         <input type="text" id="subject" name="subject">
@@ -152,6 +79,27 @@ better_form = """
         
         <input type="submit" value="Send Email">
     </form>
+    
+<script>
+    function checkDomain() {
+        var email = document.getElementById('from_email').value;
+        var domain = email.split('@')[1];
+        if (domain) {
+            fetch('/check-domain/' + domain)
+                .then(response => response.json())
+                .then(data => {
+                    var indicator = document.getElementById('domain_indicator');
+                    if (data.is_vulnerable === 1) {
+                        indicator.innerHTML = '✅';  // Tick mark emoji
+                    } else if (data.is_vulnerable === 0) {
+                        indicator.innerHTML = '❌';  // Red cross emoji
+                    } else {
+                        indicator.innerHTML = '❓';  // Question mark emoji
+                    }
+                });
+        }
+    }
+</script>
 
 </body>
 </html>
@@ -196,6 +144,22 @@ def send_email():
         return "Email sent successfully"
     except Exception as e:
         return f"Error sending email: {e}"
+
+@app.route('/check-domain/<domain>')
+def check_domain(domain):
+    try:
+        output = subprocess.check_output(["nslookup", "-type=txt", f"_dmarc.{domain}"], stderr=subprocess.STDOUT).decode()
+
+        if re.search(r';[^s]*p[s]*\s*=\s*reject\s*', output):
+            return jsonify(is_vulnerable=0)
+        elif re.search(r';[^s]*p[s]*\s*=\s*quarantine\s*', output):
+            return jsonify(is_vulnerable=0)
+        elif re.search(r';[^s]*p[s]*\s*=\s*none\s*', output):
+            return jsonify(is_vulnerable=1)
+        else:
+            return jsonify(is_vulnerable=1)
+    except subprocess.CalledProcessError:
+        return jsonify(is_vulnerable=-1)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
